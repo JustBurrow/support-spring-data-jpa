49dfeb0dc3ea15dc9854f73923b1370c727bd747
Just Burrow <just.burrow@lul.kr>
2024-02-12 17:14:21 +0900

    👷 라이브러리 배포 설정 추가.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
 
 plugins {
     java
+    `maven-publish`
+
     alias(libs.plugins.kotlin.jpa)
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.spring)
@@ -11,6 +14,9 @@ plugins {
 
 group = "kr.lul.support"
 version = "0.0.1"
+
+val properties = Properties()
+properties.load(project.rootProject.file("local.properties").inputStream())
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -39,4 +45,35 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/JustBurrow/packages")
+            credentials {
+                username = properties["github.actor"] as String?
+                    ?: System.getenv("GITHUB_ACTOR")
+                password = properties["github.token"] as String?
+                    ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+
+            groupId = "kr.lul.support"
+            artifactId = rootProject.name
+            version = "0.0.1"
+
+            pom {
+                scm {
+                    url = "https://github.com/JustBurrow/support-spring-data-jpa"
+                }
+            }
+        }
+    }
 }
